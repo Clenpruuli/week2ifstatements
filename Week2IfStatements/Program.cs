a026002bcebd0284de6982c1613290ea20366c66
Opilane TTHK <opilane@tthk.ee>
2020-09-08 13:55:45 +0300

    first commit

---
+++ Week2IfStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Week2IfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age;
+            string UserInput;
+
+            Console.WriteLine("How old are you?");
+            UserInput = Console.ReadLine();
+            Age = Int32.Parse(UserInput);
+                  if (Age < 18)
+                Console.WriteLine($"you are {Age}, you are too young.");
+                 else if (Age > 18)
+                Console.WriteLine($"You are {Age}, you are old enough.");
+                 else  
+                Console.WriteLine($"Congratulations! you are {Age}");
+
+
+ 
+
+
+
+
+
+
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your year of birth?");
+            string UserInPut;
+            UserInPut = Console.ReadLine();
+            int YearOfBirth;
+            YearOfBirth = Int32.Parse(UserInPut);
+            int age = 2020 - YearOfBirth;
+            
+            if (age < 18)
+            {
+                Console.WriteLine($"you are {age}, you are too young.");
+            } 
+            else if (age > 18)
+            {
+                Console.WriteLine($"you are {age},you are old enough.");
+            }                
+            else
+            {
+                Console.WriteLine($"Congratulations! you are {age}");
+            }
+              
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
